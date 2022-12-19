9d5e9891e2114615ca3796354b66237190ac89b2
GriffinAD <66727382+GriffinAD@users.noreply.github.com>
2022-12-19 22:26:22 +0000

    fix

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ fan = PWMOut(board.GP13, frequency=25000, duty_cycle=26214)
 adc = AnalogIn(board.A2)
 
 
-maxPercent = 100
-minPercent = 40
-minTemp = 25
-maxTemp = 30
+min_pwm_percent = 40
+max_pwm_percent = 100
+min_sensor_temperature = 25
+max_sensor_temperature = 30
 
-oldTemp = 0
+prevTemperature = 0
 
 # Thermistor specs: very generic
 r25 = 10000  # nominal resistance at 25 °C, ohms
@@ -22,10 +22,10 @@ beta = 3950  # β (25/50 °C)
 # potential divider resistor, on high side
 r_high = 5600  # ohms
 
-cal = -7
+cal = -7  # temperature offset
 
 
-def setFanSpeed(percent):
+def SetFanSpeed(percent):
     value = 65535
     if percent > 0:
         value = int(65535 / (100 / percent))
@@ -35,7 +35,7 @@ def setFanSpeed(percent):
     return value
 
 
-def getTemperature():
+def GetTemperature():
     r = r_high / (65535 / float(adc.value) - 1)
     lnr = log(r / r25)
     ts_C = int((-273.15 + 1 / (1 / 298.15 + lnr / beta)) + cal)
@@ -43,25 +43,26 @@ def getTemperature():
     return ts_C
 
 
-def calcSpeed(temp):
-    if temp < minTemp:
-        spd = minPercent
-    elif temp > maxTemp:
-        spd = maxPercent
+def CalcSpeed(temperature):
+    if temperature <= min_sensor_temperature:
+        speedVal = min_pwm_percent
+    elif temperature >= max_sensor_temperature:
+        speedVal = max_pwm_percent
     else:
-        tPerc = ((temp - minTemp)) / (maxTemp - minTemp)
-        spd = ((maxPercent - minPercent) * tPerc) + minPercent
-    # print(f"speed set: {spd} for temp: {temp} C")
-    return spd
+        tPercent = ((temperature - min_sensor_temperature)) / (max_sensor_temperature - min_sensor_temperature)
+        speedVal = ((max_pwm_percent - min_pwm_percent) * tPercent) + min_pwm_percent
+    # print(f"speed set: {speedVal} for temp: {temp} C")
+    return speedVal
 
 
 while True:
-    temp = getTemperature()
+    temperature = GetTemperature()
 
-    if oldTemp != temp:
-        spd = calcSpeed(temp)
-        setFanSpeed(spd)
-        oldTemp = temp
+    if prevTemperature != temperature:
+        speedVal = CalcSpeed(temperature)
+        SetFanSpeed(speedVal)
+        prevTemperature = temperature
     sleep(2)
+
 # for temp in range (0,101):
 # spd = calcSpeed(temp)
